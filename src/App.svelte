399d3623ed995d6a590651de2df99d65bc166ec8
Tom Wilson <twilson63@gmail.com>
2019-04-26 06:48:16 -0400

    update exercises

--- src/App.svelte
+++ src/App.svelte
@@ -8,6 +8,12 @@ import Flexbox2 from './flexbox/2.html'
 import Flexbox3 from './flexbox/3.html'
 import Flexbox4 from './flexbox/4.html'
 
+import GridMenu from './cssgrid/Menu.svelte'
+import Grid1 from './cssgrid/1.html'
+import Grid2 from './cssgrid/2.html'
+import Grid3 from './cssgrid/3.html'
+
+
 
 let page = Start
 
@@ -18,6 +24,12 @@ router('/flexbox/1', () => page = Flexbox1)
 router('/flexbox/2', () => page = Flexbox2)
 router('/flexbox/3', () => page = Flexbox3)
 router('/flexbox/4', () => page = Flexbox4)
+
+router('/cssgrid', () => page = GridMenu)
+
+router('/cssgrid/1', () => page = Grid1)
+router('/cssgrid/2', () => page = Grid2)
+router('/cssgrid/3', () => page = Grid3)
 
 
 router.start()
--- src/cssgrid/Menu.svelte
+++ src/cssgrid/Menu.svelte
@@ -3,16 +3,15 @@
     <div class="is-pulled-right">
       <a href="/">Home</a>
     </div>
-    <h1>Flexbox Exercises</h1>
+    <h1>Grid Exercises</h1>
   </div>
   <div>
     <h2>Menu</h2>
 
     <ul>
-      <li><a href="/flexbox/1">Exercise 1</a></li>
-      <li><a href="/flexbox/2">Exercise 2</a></li>
-      <li><a href="/flexbox/3">Exercise 3</a></li>
-      <li><a href="/flexbox/4">Exercise 4</a></li>
+      <li><a href="/cssgrid/1">Exercise 1</a></li>
+      <li><a href="/cssgrid/2">Exercise 2</a></li>
+      <li><a href="/cssgrid/3">Exercise 3</a></li>
     </ul>
   </div>
 </section>
